1fec71414e7fd7cb202747ef657155f4ddd15430
cloudmist <cloudmist@1230.163.com>
2021-04-20 17:21:41 +0800

    上传tab对应的插件

---
+++ components/work-list/work-list.vue
@@ -0,0 +1,23 @@
+<!-- 工作票列表组件 <work-list :list="xx"></work-list> -->
+<template>
+	<view class="work-list">
+		<view :id="'good'+good.id"  v-for="(work,index) in list" :key="good.id">
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			list: {
+				type: Array,
+				default(){
+					return []
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
